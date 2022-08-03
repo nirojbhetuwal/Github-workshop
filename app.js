3256cd1702016a646be0be04fb05bcc26b604541
nirojbhetuwal <97467091+nirojbhetuwal@users.noreply.github.com>
2022-08-03 22:39:36 +0545

    java add

---
+++ app.js
@@ -0,0 +1,15 @@
+let myBox = document.querySelector(".box")
+let myBtn = document.querySelector(".btn")
+let mytext = document.querySelector(".text")
+let comments=[];
+
+// let test = function(){
+//     let comment = mytext.value;
+//     comments.push(comment)
+//     comments.forEach(function(item){
+//         console.log(comments)
+//         let newitem = "<p>" + item +"</p>"
+//         myBox.innerHTML = newitem;
+//     })
+// }
+myBtn.addEventListener("click", test)
